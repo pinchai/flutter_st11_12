bd3ff77c137314e8ba98758028e5e87ed75fb39d
Pinchai <>
2024-07-19 08:15:07 +0700

    finished var and data type

--- lib/lesson.dart
+++ lib/lesson.dart
@@ -1,3 +1,9 @@
-void main(){
-  print("hello world");
+void main() {
+  int age = 20; // integers, range -2^63 to 2^63 - 1
+  double height = 1.85; // floating-point numbers
+  bool isFavourite = true;
+  bool isLoaded = false;
+  String name = "Nicola";
+
+
 }
